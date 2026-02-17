bf063bb2195e042f888fe6c10a272d18572e9ceb
suprajav2024-ux <supraja.v2024@teachforindia.org>
2026-02-17 22:49:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -320,4 +320,5 @@ def events():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
